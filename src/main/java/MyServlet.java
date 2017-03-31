7fc581aa09ea7bd1b58a3d2469822c961d8b2a86
lz <819468309@qq.com>
2017-03-31 17:47:12 +0800

    完善注释

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	//利用反射机制，将表单提交的内容与Bean一一对应
         UserBean bean = (UserBean)ReflectTool.initBean(req, "UserBean");
         System.out.println(bean.getUserName()+" "+bean.getPassWord());
     }
